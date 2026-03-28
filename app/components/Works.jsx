14112dc809d9a8ea50e291178cd9698169545369
AK Tanha <aktanha75@gmail.com>
2026-03-28 21:06:13 +0600

    update

--- app/components/Works.jsx
+++ app/components/Works.jsx
@@ -42,12 +42,12 @@ const Works = () => {
         initial={{ opacity: 0, y: -20 }}
         whileInView={{ opacity: 1, y: 0 }}
         transition={{ duration: 0.5, delay: 0.5 }}
-        className='text-center mb-2 text-lg font-ovo text-gray-800 dark:text-gray-200'>My Portfolio</motion.h4>
+        className='text-center mb-2 text-lg font-ovo dark:text-gray-200'>My Portfolio</motion.h4>
       <motion.h2
         initial={{ opacity: 0, y: -20 }}
         whileInView={{ opacity: 1, y: 0 }}
         transition={{ duration: 0.5, delay: 0.5 }}
-        className='text-center text-5xl font-ovo text-gray-900 dark:text-white'>My Latest Work</motion.h2>
+        className='text-center text-5xl font-ovo dark:text-white'>My Latest Work</motion.h2>
       <motion.p
         initial={{ opacity: 0 }}
         whileInView={{ opacity: 1 }}
@@ -69,7 +69,7 @@ const Works = () => {
             style={{ backgroundImage: `url(${projects.bgImage})` }}>
             <div className='bg-white dark:bg-slate-800 w-10/12 rounded-md absolute bottom-5 left-1/2 -translate-x-1/2 py-3 px-5 flex items-center justify-between duration-500 group-hover:bottom-7'>
               <div>
-                <h2 className='font-semibold text-gray-900 dark:text-gray-100'>{projects.title}</h2>
+                <h2 className='font-semibold dark:text-gray-100'>{projects.title}</h2>
                 <p className='text-sm text-gray-700 dark:text-gray-300'>{projects.description}</p>
               </div>
               <div className='border rounded-full border-black dark:border-gray-400 w-9 aspect-square
