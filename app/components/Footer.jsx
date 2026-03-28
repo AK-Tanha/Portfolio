b88b90623012664cd95389cff771f9c55fd6ee80
AK Tanha <aktanha75@gmail.com>
2026-03-28 19:21:02 +0600

    footer updated

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -17,7 +17,7 @@ const Footer = () => {
   return (
     <div className='mt-20'>
       <div className='text-center'>
-        <Image src={isDark ? assets.logo_dark : assets.aktanha_logo} alt='logo' className='w-36 mx-auto mb-2'/> 
+        <Image src={isDark ? assets.aktanha_logo_dark : assets.aktanha_logo} alt='logo' className='w-36 mx-auto mb-2'/> 
       </div>
       <div className='w-max flex items-center gap-2 mx-auto'> 
         <Image src={assets.mail_icon} alt='' className='w-6'/>
